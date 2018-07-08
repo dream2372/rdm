2ca66feb9aba9bdd872d3cf5737b30dcbf062c5c
Chris Souers <csouers@gmail.com>
2018-07-08 12:50:16 -0400

    hondacan

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -3,7 +3,7 @@ import struct
 import common.numpy_fast as np
 from selfdrive.config import Conversions as CV
 from selfdrive.car.honda.values import CAR
-#from selfdrive.car.honda.carstate import CarState, CS
+from selfdrive.car.honda.carstate import CarState
 
 # *** Honda specific ***
 def can_cksum(mm):
