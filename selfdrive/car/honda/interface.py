c15957bea7b0a9ce1c48b8b9928d36ba91294420
Chris Souers <csouers@gmail.com>
2018-07-31 20:45:18 -0400

    actually allow gas!

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -354,7 +354,7 @@ class CarInterface(object):
     ret.steerMaxV = [1.]   # max steer allowed
 
     ret.gasMaxBP = [0.]  # m/s
-    ret.gasMaxV = [0.6] if ret.enableGasInterceptor else [0.] # max gas allowed
+    ret.gasMaxV = [0.6] if ret.enableGasInterceptor or ret.visionRadar else [0.] # max gas allowed
     ret.brakeMaxBP = [5., 20.]  # m/s
     ret.brakeMaxV = [1., 0.8]   # max brake allowed
 
