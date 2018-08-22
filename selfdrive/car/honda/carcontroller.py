826df69ce003be5d2057c9166f5a8f56a419a7ea
Chris Souers <csouers@gmail.com>
2018-08-22 15:14:31 -0400

    max accel set back

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -12,7 +12,7 @@ from selfdrive.services import service_list
 
 # Accel limits from toyota
 ACCEL_HYST_GAP = 0.02  # was 0.02 # don't change accel command for small oscilalitons within this value
-ACCEL_MAX = 0.5  # 1.5 m/s2
+ACCEL_MAX = 1.5  # 1.5 m/s2
 ACCEL_MIN = -3.0 # 3   m/s2
 ACCEL_SCALE = max(ACCEL_MAX, -ACCEL_MIN)
 
