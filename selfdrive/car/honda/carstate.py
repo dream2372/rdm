70bba83155d43a978d1d51726c1ce9e7e63c7609
Chris Souers <csouers@gmail.com>
2019-08-18 23:27:38 -0400

    Update carstate.py

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -96,7 +96,7 @@ def get_can_signals(CP):
                 ("MAIN_ON", "SCM_FEEDBACK", 0),
                 ("CRUISE_CONTROL_LABEL", "ACC_HUD", 0),
                 ("EPB_STATE", "EPB_STATUS", 0),
-                ("CRUISE_SPEED", "ACC_HUD", 0)
+                ("CRUISE_SPEED", "ACC_HUD", 0),
                 ("BRAKE_ERROR_1", "STANDSTILL", 1),
                 ("BRAKE_ERROR_2", "STANDSTILL", 1)]
     checks += [("GAS_PEDAL_2", 100),
