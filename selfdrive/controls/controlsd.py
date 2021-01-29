ceb9697b198de5b4e8c5757318ca9a99be513405
Chris Souers <csouers@gmail.com>
2021-01-29 17:51:36 -0500

    try aTarget from controlsstate again

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -465,7 +465,7 @@ class Controls:
 
     if not self.read_only:
       # send car controls over can
-      can_sends = self.CI.apply(CC, self.sm['plan'].aTarget)
+      can_sends = self.CI.apply(CC, float(a_acc))
       self.pm.send('sendcan', can_list_to_can_capnp(can_sends, msgtype='sendcan', valid=CS.canValid))
 
     force_decel = (self.sm['dMonitoringState'].awarenessStatus < 0.) or \
