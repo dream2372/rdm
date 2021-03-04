csouers/openpilot

Body Harness Support
These commit messages are broken. Clean up

Update .gitmodules

start

Update .gitmodules

modules

new test. not run on device yet

new test. not run on device yet

hard code for now

closer

Update athenad.py

bodyd

update bodyd

Set ignition off safety to elm_327.

.gitmodules

bodyd and test script

panda module

update bodyd

fix boardd

EPS Changes

update

bump cereal

Update cereal

modules

bump opendbc

x

start major files

Body Harness Support

Update .gitmodules

start

Update .gitmodules

modules

new test. not run on device yet

new test. not run on device yet

hard code for now

closer

Update athenad.py

bodyd

update bodyd

Set ignition off safety to elm_327.

.gitmodules

bodyd and test script

panda module

update bodyd

fix boardd

EPS Changes

update

bump cereal

Update cereal

modules

bump opendbc

x

start major files

push. is broken

push. is broken

Update manager.py

split off load_car and other things

nightly push

push

name update

rename trunk frame

Update bodyd_helpers.py

mostly working

better

switch to pubmaster. this works

print and rate fix

Update bodyd.py

Update .gitmodules

modules

selfdrive/boardd/boardd.cc
@@ -309,8 +309,8 @@ void can_health_thread(bool spoofing_started) {
     }
 
     // set safety mode to NO_OUTPUT when car is off. ELM327 is an alternative if we want to leverage athenad/connect
-    if (!ignition && (health.safety_model != (uint8_t)(cereal::CarParams::SafetyModel::NO_OUTPUT))) {
-      panda->set_safety_model(cereal::CarParams::SafetyModel::NO_OUTPUT);
+    if (!ignition && (health.safety_model != (uint8_t)(cereal::CarParams::SafetyModel::ELM327))) {
+      panda->set_safety_model(cereal::CarParams::SafetyModel::ELM327);
     }
 #endif
 

dream2372/rdm

selfdrive/boardd/boardd.cc
@@ -309,8 +309,8 @@ void panda_state_thread(bool spoofing_started) {
     }
 
     // set safety mode to NO_OUTPUT when car is off. ELM327 is an alternative if we want to leverage athenad/connect
-    if (!ignition && (pandaState.safety_model != (uint8_t)(cereal::CarParams::SafetyModel::NO_OUTPUT))) {
-      panda->set_safety_model(cereal::CarParams::SafetyModel::NO_OUTPUT);
+    if (!ignition && (pandaState.safety_model != (uint8_t)(cereal::CarParams::SafetyModel::ELM327))) {
+      panda->set_safety_model(cereal::CarParams::SafetyModel::ELM327);
     }
 #endif
 