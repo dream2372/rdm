021e47b409927951da24af6e80b3e2f1831f3c31
Chris Souers <csouers@gmail.com>
2021-05-03 10:44:12 -0400

    clarify the bodyd requirement

--- selfdrive/car/honda/bodyvalues.py
+++ selfdrive/car/honda/bodyvalues.py
@@ -1,12 +1,13 @@
 from selfdrive.body.lib.bodyd_helpers import dbc_dict
 
 
-# supported cars here. Mimic car/make/values.py
+# supported cars here. Mimic car/make/values.py. Must have verified signals against a test route.
 # this might not work out long term as some body messages could change between model years where ADAS doesn't
 class CAR:
   ACCORD = "HONDA ACCORD 2018 SPORT 2T"
   CIVIC_BOSCH = 'HONDA CIVIC HATCHBACK 2017 SEDAN/COUPE 2019'
   CIVIC_BOSCH_DIESEL = "HONDA CIVIC SEDAN 1.6 DIESEL"
+
 
 # id, time (0), data, bus
 # CAN_COMMANDS = {
@@ -15,6 +16,7 @@ class CAR:
 #     'LOCK': '[0x0ef81218, 0, b"\x01\x00", 0]',  # keyfob
 #   }
 # }
+
 
 DBC = {
   CAR.ACCORD: dbc_dict('honda_accord_s2t_2018_body_generated'),
