89a7d18fdb677eb6aa1624c028adca6108d36104
Chris Souers <csouers@gmail.com>
2021-08-10 00:56:43 -0400

    already in get_std_params() in car/interfaces.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -477,8 +477,6 @@ class CarInterface(CarInterfaceBase):
       ret.brakeMaxV = [1., 0.8]   # max brake allowed
       ret.startAccel = 0.5
 
-    ret.stoppingControl = True
-
     ret.steerActuatorDelay = 0.1
     ret.steerRateCost = 0.5
     ret.steerLimitTimer = 0.8
