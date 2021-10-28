4662429ceca70bbaaa216027849c63c2f928375c
Chris Souers <csouers@gmail.com>
2021-10-28 17:14:44 -0400

    0.5s here causes us to stop too far behind and then creep forward

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -86,9 +86,10 @@ class CarInterface(CarInterfaceBase):
         # ret.longitudinalTuning.kiV = [0.10, 0.05]
 
       else:
+        # 20hz
         ret.longitudinalTuning.kpV = [0.25]
         ret.longitudinalTuning.kiV = [0.05]
-      ret.longitudinalActuatorDelayUpperBound = 0.5 # s
+      ret.longitudinalActuatorDelayUpperBound = 0.2 # s
     else:
       # default longitudinal tuning for all hondas
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
