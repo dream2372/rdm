79673044d292ede2ed5b3f2be7dbea78d3777813
Chris Souers <csouers@gmail.com>
2021-10-30 08:46:18 -0400

    is this any better?

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -76,8 +76,16 @@ class LongControl():
       v_target_upper = interp(CP.longitudinalActuatorDelayUpperBound, T_IDXS[:CONTROL_N], long_plan.speeds)
       a_target_upper = 2 * (v_target_upper - long_plan.speeds[0])/CP.longitudinalActuatorDelayUpperBound - long_plan.accels[0]
 
-      v_target = min(v_target_lower, v_target_upper)
-      a_target = min(a_target_lower, a_target_upper)
+      if v_target_upper < v_target_lower:
+        a_target = a_target_upper
+        v_target = v_target_upper
+      elif v_target_lower < v_target_upper:
+        a_target = a_target_lower
+        v_target = v_target_lower
+      else:
+        # this should only happen if they're the same. failsafe
+        v_target = min(v_target_lower, v_target_upper)
+        a_target = min(a_target_lower, a_target_upper)
 
       v_target_future = long_plan.speeds[-1]
     else:
