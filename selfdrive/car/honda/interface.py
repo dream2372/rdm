b377c4aa7aae3c19bc8c84df299656aefb6a8543
Chris Souers <csouers@gmail.com>
2021-10-31 16:33:01 -0400

    assume we don't have a radar to disable

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -6,7 +6,7 @@ from common.params import Params
 from selfdrive.car.honda.values import CarControllerParams, CruiseButtons, CAR, HONDA_BOSCH, HONDA_BOSCH_ALT_BRAKE_SIGNAL
 from selfdrive.car import STD_CARGO_KG, CivicParams, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint, get_safety_config
 from selfdrive.car.interfaces import CarInterfaceBase
-from selfdrive.car.disable_ecu import disable_ecu
+# from selfdrive.car.disable_ecu import disable_ecu
 from selfdrive.config import Conversions as CV
 
 
@@ -347,8 +347,9 @@ class CarInterface(CarInterfaceBase):
 
   @staticmethod
   def init(CP, logcan, sendcan):
-    if CP.carFingerprint in HONDA_BOSCH and CP.openpilotLongitudinalControl and not Params().get_bool("TeslaRadarActivate"):
-      disable_ecu(logcan, sendcan, bus=1, addr=0x18DAB0F1, com_cont_req=b'\x28\x83\x03')
+    pass
+    # if CP.carFingerprint in HONDA_BOSCH and CP.openpilotLongitudinalControl and not Params().get_bool("TeslaRadarActivate"):
+    #   disable_ecu(logcan, sendcan, bus=1, addr=0x18DAB0F1, com_cont_req=b'\x28\x83\x03')
 
   # returns a car.CarState
   def update(self, c, can_strings):
