38e86ad3d143fbc164ca724a47fa3d4719ae9594
Chris Souers <csouers@gmail.com>
2023-05-07 03:12:19 -0400

    add packer. cleaner

--- selfdrive/car/honda/ibooster/idle_test.py
+++ selfdrive/car/honda/ibooster/idle_test.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
 import cereal.messaging as messaging
+
+from opendbc.can.packer import CANPacker
+
 from selfdrive.boardd.boardd import can_list_to_can_capnp
+
 import time
 
 
-packer = ()
-
-def FakeHonda(pm, frame, idx):
+def FakeHonda(pm, frame, idx, packer):
   can_sends = []
+
   ###### 100hz #####
   for bus in [0,2]:
     # can_sends.append(packer.make_can_msg("SRS_KINEMATICS", bus, {})) # 148
-    can_sends.append(packer.make_can_msg("PCM_METERFORWARD_GAS_PEDAL_2", bus, {})) # 304
+    can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_GAS_PEDAL_2", bus, {})) # 304
     # can_sends.append(packer.make_can_msg("EPS_STEERING_SENSORS_A", bus, {})) # 330
-    can_sends.append(packer.make_can_msg("TCM_METERFORWARD_ENGINE_DATA", bus, {})) # 344
-    # can_sends.append(packer.make_can_msg("PCM_METERFORWARD_POWERTRAIN_DATA", bus, {})) # 380
-    # can_sends.append(packer.make_can_msg("TCM_METERFORWARD_GEARBOX_CVT", bus, {})) # 401
+    can_sends.append(packer.make_can_msg("TCM_GATEWAYFORWARD_ENGINE_DATA", bus, {})) # 344
+    # can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_POWERTRAIN_DATA", bus, {})) # 380
+    # can_sends.append(packer.make_can_msg("TCM_GATEWAYFORWARD_GEARBOX_CVT", bus, {})) # 401
     if bus == 0:
       # bus 0
       can_sends.append(packer.make_can_msg("RADAR_STEERING_CONTROL", 0, {})) # 228
@@ -34,19 +37,19 @@ def FakeHonda(pm, frame, idx):
       # can_sends.append(packer.make_can_msg("VSA_STATUS", bus, {})) # 420
       can_sends.append(packer.make_can_msg("OBD2_TCM_1A7", bus, {})) # 423
       can_sends.append(packer.make_can_msg("VSA_STANDSTILL", bus, {})) # 432
-      # can_sends.append(packer.make_can_msg("VSA_METERFORWARD_EPB_STATUS", bus, {})) # 450
+      # can_sends.append(packer.make_can_msg("VSA_GATEWAYFORWARD_EPB_STATUS", bus, {})) # 450
       # can_sends.append(packer.make_can_msg("VSA_WHEEL_SPEEDS", bus, {})) # 464
       # can_sends.append(packer.make_can_msg("RADAR_ACC_CONTROL", bus, {})) # 479
       can_sends.append(packer.make_can_msg("RADAR_ACC_CONTROL_ON", bus, {})) # 495
       if bus == 0:
         can_sends.append(packer.make_can_msg("VSA_WHEEL_TICKS", bus, {})) # 441
-        # can_sends.append(packer.make_can_msg("PCM_METERFORWARD_1DD", bus, {})) # 477
+        # can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_1DD", bus, {})) # 477
         can_sends.append(packer.make_can_msg("VSA_KINEMATICS", bus, {})) # 490
       else:
-        can_sends.append(packer.make_can_msg("VSA_METERFORWARD_1AC", bus, {})) # 428
-        can_sends.append(packer.make_can_msg("PCM_METERFORWARD_1D6", bus, {})) # 470
-        can_sends.append(packer.make_can_msg("PCM_METERFORWARD_ENGINE_DATA_2", bus, {})) # 476
-        can_sends.append(packer.make_can_msg("PCM_METERFORWARD_ACC_CONTROL_ON_2", bus, {})) # 493
+        can_sends.append(packer.make_can_msg("VSA_GATEWAYFORWARD_1AC", bus, {})) # 428
+        can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_1D6", bus, {})) # 470
+        can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_ENGINE_DATA_2", bus, {})) # 476
+        can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_ACC_CONTROL_ON_2", bus, {})) # 493
         can_sends.append(packer.make_can_msg("OBD2_IDK_1FB", bus, {})) # 507
     
   ###### 25hz #####
@@ -66,17 +69,17 @@ def FakeHonda(pm, frame, idx):
       # can_sends.append(packer.make_can_msg("SRS_SEATBELT_STATUS", bus, {})) # 773
       can_sends.append(packer.make_can_msg("METER_CAR_SPEED", bus, {})) # 777
       can_sends.append(packer.make_can_msg("RADAR_ACC_HUD", bus, {})) # 780
-      can_sends.append(packer.make_can_msg("PCM_METERFORWARD_320", bus, {})) # 800
-      # can_sends.append(packer.make_can_msg("PCM_METERFORWARD_CRUISE", bus, {})) # 804
+      can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_320", bus, {})) # 800
+      # can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_CRUISE", bus, {})) # 804
       # can_sends.append(packer.make_can_msg("METER_SCM_FEEDBACK", bus, {})) # 806
-      can_sends.append(packer.make_can_msg("PCM_METERFORWARD_328", bus, {})) # 808
+      can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_328", bus, {})) # 808
       # can_sends.append(packer.make_can_msg("RADAR_LKAS_HUD", bus, {})) # 829
       # can_sends.append(packer.make_can_msg("CAMERA_MESSAGES", bus, {})) # 862
       # can_sends.append(packer.make_can_msg("METER_STALK_STATUS", bus, {})) # 884
       can_sends.append(packer.make_can_msg("METER_STALK_STATUS_2", bus, {})) # 891
       # can_sends.append(packer.make_can_msg("RADAR_HUD", bus, {})) # 927
       if bus == 2:
-        # can_sends.append(packer.make_can_msg("VSA_METERFORWARD_31B", bus, {})) # 795
+        # can_sends.append(packer.make_can_msg("VSA_GATEWAYFORWARD_31B", bus, {})) # 795
         can_sends.append(packer.make_can_msg("OBD2_PCM_32F", bus, {})) # 815
         can_sends.append(packer.make_can_msg("OBD2_PCM_339", bus, {})) # 825
         can_sends.append(packer.make_can_msg("OBD2_METER_371", bus, {})) # 881
@@ -92,23 +95,25 @@ def FakeHonda(pm, frame, idx):
     #     can_sends.append(packer.make_can_msg("VSA_3D9", bus, {})) # 985
     #   else:
     #     can_sends.append(packer.make_can_msg("OBD2_PCM_3D7", bus, {})) # 983
+
   ###### 3hz #####
   if (frame % 33) == 0:
     for bus in [0,2]:
-      # can_sends.append(packer.make_can_msg("TCM_METERFORWARD_403", bus, {})) # 1027
+      # can_sends.append(packer.make_can_msg("TCM_GATEWAYFORWARD_403", bus, {})) # 1027
       can_sends.append(packer.make_can_msg("METER_DOORS_STATUS", bus, {})) # 1029
-      # can_sends.append(packer.make_can_msg("PCM_METERFORWARD_40C", bus, {})) # 1036
-      # can_sends.append(packer.make_can_msg("TCM_METERFORWARD_40F", bus, {})) # 1039
+      # can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_40C", bus, {})) # 1036
+      # can_sends.append(packer.make_can_msg("TCM_GATEWAYFORWARD_40F", bus, {})) # 1039
       can_sends.append(packer.make_can_msg("NEW_MSG_445", bus, {})) # 1093
-      can_sends.append(packer.make_can_msg("PCM_METERFORWARD_454", bus, {})) # 1108
+      can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_454", bus, {})) # 1108
     if bus == 0:
-      can_sends.append(packer.make_can_msg("PCM_METERFORWARD_400", bus, {})) # 1024
+      can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_400", bus, {})) # 1024
     else:
       can_sends.append(packer.make_can_msg("OBD2_METER_428", bus, {})) # 1064
       can_sends.append(packer.make_can_msg("OBD2_METER_444", bus, {})) # 1092
       # can_sends.append(packer.make_can_msg("OBD2_IDK_45B", bus, {})) # 1115
       can_sends.append(packer.make_can_msg("OBD2_PCM_465", bus, {})) # 1125
       can_sends.append(packer.make_can_msg("OBD2_PCM_467", bus, {})) # 1127
+
   ###### 2hz #####
   if (frame % 50) == 0:
     for bus in [0,2]:
@@ -117,11 +122,13 @@ def FakeHonda(pm, frame, idx):
       can_sends.append(packer.make_can_msg("METER_52A", bus, {})) # 1322
       can_sends.append(packer.make_can_msg("EPS_MAYBE_551", bus, {})) # 1361
       can_sends.append(packer.make_can_msg("SRS_555", bus, {})) # 1365
-      can_sends.append(packer.make_can_msg("PCM_METERFORWARD_661", bus, {})) # 1633
+      can_sends.append(packer.make_can_msg("PCM_GATEWAYFORWARD_661", bus, {})) # 1633
     else:
       can_sends.append(packer.make_can_msg("OBD2_METER_510", bus, {})) # 1296
+
   ###### 1hz #####
   if (frame % 100) == 0:
+    print(f"Time is {frame}")
     can_sends.append(packer.make_can_msg("VSA_590", bus, {})) # 1424
     # can_sends.append(packer.make_can_msg("RADAR_640", bus, {})) # 1600
     # can_sends.append(packer.make_can_msg("RADAR_641", bus, {})) # 1601
@@ -142,22 +149,22 @@ def FakeHonda(pm, frame, idx):
     idx = (idx+1) % 4
 
   if len(can_sends) != 0:
-    # print(can_sends)
     pm.send('sendcan', can_list_to_can_capnp(can_sends, msgtype='sendcan', valid=True))
 
   return idx
 
 def nav_thread():
-    idx = 0
-    frame = 0
+  idx = 0
+  frame = 0
 
-    pm = messaging.PubMaster(['sendcan'])
+  packer = CANPacker('HONDA_MASTER')
+  pm = messaging.PubMaster(['sendcan'])
 
-    while 1:
-      # 100hz loop
-      frame += 1
-      idx = FakeHonda(pm, frame, idx)
-      time.sleep(0.01)
+  while 1:
+    # 100hz loop
+    frame += 1
+    idx = FakeHonda(pm, frame, idx, packer)
+    time.sleep(0.01)
 
 if __name__ == '__main__':
   nav_thread()
